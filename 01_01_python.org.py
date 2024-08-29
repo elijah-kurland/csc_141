7393f5804d90c31460bf81dcd9b5e7ee9f7826d9
Elijah-Kurland <captainrex017@gmail.com>
2024-08-29 11:09:27 -0400

    Add files via upload

---
+++ 01_01_python.org.py
@@ -0,0 +1,6 @@
+'''
+A few things I found on the python website include a getting started page that teaches newcomers the basics.
+There is also a link that explores different jobs that require python knowledge and what places are hiring. 
+Finally there is a page dedicated to success stories that explores a broad spectrum of people and their own 
+success with python based careers.
+'''
