4186a8f35beec7490c73578baaf10e038b7dcef1
Elijah-Kurland <captainrex017@gmail.com>
2024-08-29 11:08:14 -0400

    Add files via upload

---
+++ 01_03_infinite_skills.py
@@ -0,0 +1,5 @@
+'''
+#1 Adding an AI based program to put directions into that will produce a code. This could also help with fixing errors and adding comments.
+#2 A program that acts as a spell check that wouldn't autocorrect (incase something was intentionally misspelled) but would underline or highlight the word(s) being misspelled.
+#3 A program teaching tool that would be step by step and would folow along with the learner. It would only move on to the next step if the learner wrote the code correctly.
+'''
