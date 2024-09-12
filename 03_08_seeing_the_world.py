df9a59069f3eb72e5d0ffab1443aab2945eb8403
Elijah-Kurland <captainrex017@gmail.com>
2024-09-12 11:28:02 -0400

    Add files via upload
    These are my assignments from chapter 3.

---
+++ 03_08_seeing_the_world.py
@@ -0,0 +1,27 @@
+#Creates a new list of locations.
+places = ['Tokyo', 'Ireland', 'Billund', 'Iceland', 'Canada']
+print (places)
+#Sorts the places into alphebetical order.
+print (sorted(places))
+
+print (places)
+
+print(sorted(places,reverse=True))
+
+print (places)
+
+places.reverse()
+
+print (places)
+
+places.reverse()
+
+print (places)
+
+places.sort()
+
+print (places)
+#Reverses the sorted places.
+places.sort(reverse=True)
+
+print (places)
---
+++ 03_09_dinner_guests.py
@@ -0,0 +1,4 @@
+#This code will count the number of items in the list and then print the number. In this case it will print 3.
+guests = ['Ryan Reynolds', 'Chris Pratt', 'Ewan McGreggor']
+count_guests = len(guests)
+print ("{} is the number of guests invited".format(count_guests))
