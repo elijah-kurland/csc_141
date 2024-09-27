f6b275dec66df0bd02f5f70fbb11be068b084c79
Elijah-Kurland <captainrex017@gmail.com>
2024-09-27 09:17:00 -0400

    Delete 03_09_dinner_guests.py

--- 03_09_dinner_guests.py
+++
@@ -1,4 +0,0 @@
-#This code will count the number of items in the list and then print the number. In this case it will print 3.
-guests = ['Ryan Reynolds', 'Chris Pratt', 'Ewan McGreggor']
-count_guests = len(guests)
-print ("{} is the number of guests invited".format(count_guests))
