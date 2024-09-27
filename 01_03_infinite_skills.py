5a4e5a65df22c68a2762cfc5594c4ddd9d953184
Elijah-Kurland <captainrex017@gmail.com>
2024-09-27 09:14:51 -0400

    Delete 01_03_infinite_skills.py

--- 01_03_infinite_skills.py
+++
@@ -1,7 +0,0 @@
-'''
-One thing I would like to learn is adding an AI based program to put directions into that will produce a code. This could also help 
-with fixing errors and adding comments. Another program that could be helpful is one that acts as a spell check that wouldn't 
-autocorrect (incase something was intentionally misspelled) but would underline or highlight the word(s) being misspelled. 
-Finally, a program teaching tool that would be step by step and would follow along with the learner. 
-It would only move on to the next step if the learner wrote the code correctly.
-'''
