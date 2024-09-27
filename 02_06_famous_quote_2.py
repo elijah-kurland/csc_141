6d9feb336738f4146ee95d2c6ea4a1484277050c
Elijah-Kurland <captainrex017@gmail.com>
2024-09-27 09:23:05 -0400

    Delete 02_06_famous_quote_2.py

--- 02_06_famous_quote_2.py
+++
@@ -1,5 +0,0 @@
-famous_person = "Ashley Eckstein"
-message = f' {famous_person} once said, "It is a new day, a new beginning"'
-print (message)
-
-#Sept. 3, 2024 (This code implements the name of the famouse person into the message to make a complete sentence.)
