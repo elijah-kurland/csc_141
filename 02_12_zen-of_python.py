a2912542a2d2997c7976ed92db0671090ca7c8f4
Elijah-Kurland <captainrex017@gmail.com>
2024-09-27 09:22:24 -0400

    Delete 02_12_zen-of_python.py

--- 02_12_zen-of_python.py
+++
@@ -1,9 +0,0 @@
-'''
-A few things I learned from Zen of Python include that simple is better than complex.
-You want your program to look as simple as possible for better understanding and less 
-chance of anything going wrong in the code. Readability is also very important, 
-not just for the actual interpreter but for programmers as well to be able to read and 
-figure out what the code means. Finally, it is important to explain what your code is doing. 
-This helps both you and other programmers help to understand the code. This also can help 
-if you are returning to your code after a break and need a sense of guidance on what to do.
-'''
