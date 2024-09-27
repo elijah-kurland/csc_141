b368075a2ee5b2338e3f5591e8aaaaf71c031ce9
Elijah-Kurland <captainrex017@gmail.com>
2024-09-27 09:20:23 -0400

    Delete 02_08_file_extensions.py

--- 02_08_file_extensions.py
+++
@@ -1,6 +0,0 @@
-filename = 'python_notes.txt'
-simple_filename = filename.removesuffix('.txt')
-
-print (simple_filename)
-
-#Sept. 3, 2024 (This code deletes the unnecessary ".txt" from the filename. This is used mostly with websites and URLs.)
