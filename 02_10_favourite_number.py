2b09488d187708105dc292fa1fbb3f2bbdf861df
Elijah-Kurland <captainrex017@gmail.com>
2024-09-27 09:15:24 -0400

    Delete 02_10_favourite_number.py

--- 02_10_favourite_number.py
+++
@@ -1,5 +0,0 @@
-favorite_number = '501'
-message = f'My favorite number is {favorite_number}'
-print (message)
-
-#Sept. 3, 2024 (This code uses the fstring to implement the favorite number into the printed message.)
