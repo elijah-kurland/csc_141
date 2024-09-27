91eeda74e06f077ae7cf93727b44b8bcb6486536
Elijah-Kurland <captainrex017@gmail.com>
2024-09-27 09:21:41 -0400

    Delete 04_05_summing_a_million.py

--- 04_05_summing_a_million.py
+++
@@ -1,6 +0,0 @@
-#This prints the minimum(1), the maximum(1,000,000), and the sum(500,000,500,000)
-numbers = list(range(1,1_000_001))
-
-print(min(numbers))
-print(max(numbers))
-print(sum(numbers))
