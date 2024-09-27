53e61a5500a55c23d7e896e1c2a89b03fb09a71a
Elijah-Kurland <captainrex017@gmail.com>
2024-09-27 09:20:54 -0400

    Delete 03_11_intentional error.py

--- 03_11_intentional error.py
+++
@@ -1,4 +0,0 @@
-'''I made an error on 03_07_shrinking_guest_list but was able to fix it.
-The problem had to do with an index error where it couldn't find the last number
-I implemented. To fix this I changed the style of the entire code and tried 
-something different which seemed to work. I did have to look up how to fix my error though.'''
